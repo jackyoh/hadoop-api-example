0f3ae8ce5594457d65a3ad30fafdcf9a8412ff33
jack <jack@is-land.com.tw>
2016-04-12 10:46:06 +0800

    add mapreduce unit test code

---
+++ src/test/java/com/hadoop/example/mapreduce/WordCountTest.java
@@ -0,0 +1,57 @@
+package com.hadoop.example.mapreduce;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+public class WordCountTest {
+
+	@Test
+	public void testMapper() throws IOException{
+		WordCount.TokenizerMapper tokenMapper = new WordCount.TokenizerMapper();
+		MapDriver<LongWritable, Text, Text, IntWritable> mapDriver = MapDriver.newMapDriver(tokenMapper);
+		
+		mapDriver.withInput(new LongWritable(), new Text("aaaa,bbbb"));
+		mapDriver.withOutput(new Text("aaaa"), new IntWritable(1));
+		mapDriver.withOutput(new Text("bbbb"), new IntWritable(1));
+		mapDriver.runTest();
+	}
+	
+	@Test
+	public void testReduce() throws IOException{
+		List<IntWritable> values = new ArrayList<IntWritable>();
+		values.add(new IntWritable(1));
+		values.add(new IntWritable(1));
+		
+		WordCount.IntSumReducer intSumReducer = new WordCount.IntSumReducer();
+		ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver = ReduceDriver.newReduceDriver(intSumReducer);
+		reduceDriver.withInput(new Text("aaaa"), values);
+		reduceDriver.withOutput(new Text("aaaa"), new IntWritable(2));
+		reduceDriver.runTest();		
+	}
+	
+	@Test
+	public void testMapReduce() throws IOException{
+		WordCount.TokenizerMapper tokenMapper = new WordCount.TokenizerMapper();
+		WordCount.IntSumReducer intSumReducer = new WordCount.IntSumReducer();
+		
+		MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver 
+		                                            = MapReduceDriver.newMapReduceDriver(tokenMapper, intSumReducer);
+		
+		mapReduceDriver.withInput(new LongWritable(), new Text("aaaa,bbbb,cccc,dddd"));  
+	    mapReduceDriver.withOutput(new Text("aaaa"), new IntWritable(1));
+	    mapReduceDriver.withOutput(new Text("bbbb"), new IntWritable(1));
+	    mapReduceDriver.withOutput(new Text("cccc"), new IntWritable(1));
+	    mapReduceDriver.withOutput(new Text("dddd"), new IntWritable(1));
+		mapReduceDriver.runTest();
+	}
+
+}
