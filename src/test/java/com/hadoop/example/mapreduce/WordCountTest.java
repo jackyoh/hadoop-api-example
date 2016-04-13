c7df707a86b972d3ac3199a452a717f1814c1c63
jack <jack@is-land.com.tw>
2016-04-13 09:38:52 +0800

    Modify build.gradle for build jar exception

--- src/test/java/com/hadoop/example/mapreduce/WordCountTest.java
+++ src/test/java/com/hadoop/example/mapreduce/WordCountTest.java
@@ -46,8 +46,8 @@ public class WordCountTest {
 		MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver 
 		                                            = MapReduceDriver.newMapReduceDriver(tokenMapper, intSumReducer);
 		
-		mapReduceDriver.withInput(new LongWritable(), new Text("aaaa,bbbb,cccc,dddd"));  
-	    mapReduceDriver.withOutput(new Text("aaaa"), new IntWritable(1));
+		mapReduceDriver.withInput(new LongWritable(), new Text("aaaa,bbbb,cccc,aaaa,dddd"));  
+	    mapReduceDriver.withOutput(new Text("aaaa"), new IntWritable(2));
 	    mapReduceDriver.withOutput(new Text("bbbb"), new IntWritable(1));
 	    mapReduceDriver.withOutput(new Text("cccc"), new IntWritable(1));
 	    mapReduceDriver.withOutput(new Text("dddd"), new IntWritable(1));
